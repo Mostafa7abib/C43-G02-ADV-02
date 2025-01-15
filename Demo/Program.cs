667f15db3882f371dd720102e6e4e38042eb522e
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-15 12:56:46 +0200

    Video 2- Generic Collection [List]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -19,30 +19,69 @@ namespace Demo
         static void Main(string[] args)
         {
             #region Video 1
-            ArrayList arrayList = new ArrayList();
-            Console.WriteLine($"Count ==> {arrayList.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList.Capacity}");
-            arrayList.Add(1);
-            Console.WriteLine($"Count ==> {arrayList.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList.Capacity}");
-            arrayList.AddRange(new int[] { 2, 3, 4 });
-            Console.WriteLine($"Count ==> {arrayList.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList.Capacity}");
-            ArrayList arrayList1 = new ArrayList(5);
-            Console.WriteLine($"Count ==> {arrayList1.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
-            arrayList1.AddRange(new int[] { 1, 2, 3, 4, 5 });
-            Console.WriteLine($"Count ==> {arrayList1.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
-            arrayList1.Add(6);
-            Console.WriteLine($"Count ==> {arrayList1.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
-            arrayList1.TrimToSize();
-            Console.WriteLine($"Count ==> {arrayList1.Count}");
-            Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
-            arrayList1.Add("Mostafa");
-            int? Sum = SumArrayList(arrayList1); 
-            Console.WriteLine(Sum);
+            //ArrayList arrayList = new ArrayList();
+            //Console.WriteLine($"Count ==> {arrayList.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList.Capacity}");
+            //arrayList.Add(1);
+            //Console.WriteLine($"Count ==> {arrayList.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList.Capacity}");
+            //arrayList.AddRange(new int[] { 2, 3, 4 });
+            //Console.WriteLine($"Count ==> {arrayList.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList.Capacity}");
+            //ArrayList arrayList1 = new ArrayList(5);
+            //Console.WriteLine($"Count ==> {arrayList1.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
+            //arrayList1.AddRange(new int[] { 1, 2, 3, 4, 5 });
+            //Console.WriteLine($"Count ==> {arrayList1.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
+            //arrayList1.Add(6);
+            //Console.WriteLine($"Count ==> {arrayList1.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
+            //arrayList1.TrimToSize();
+            //Console.WriteLine($"Count ==> {arrayList1.Count}");
+            //Console.WriteLine($"Capacity ==> {arrayList1.Capacity}");
+            //arrayList1.Add("Mostafa");
+            //int? Sum = SumArrayList(arrayList1); 
+            //Console.WriteLine(Sum);
+            #endregion
+
+            #region Video 2
+            List<int> Numbers = new List<int>();
+            Console.WriteLine($"Count ==> {Numbers.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers.Capacity}");
+            Console.WriteLine();
+            Numbers.Add(1);
+            Console.WriteLine($"Count ==> {Numbers.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers.Capacity}");
+            Console.WriteLine();
+            Numbers.AddRange(new int[] { 2, 3, 4 });
+            Console.WriteLine($"Count ==> {Numbers.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers.Capacity}");
+            Console.WriteLine();
+            Numbers.Add(5);
+            Console.WriteLine($"Count ==> {Numbers.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers.Capacity}");
+            Console.WriteLine(); 
+            Numbers.TrimExcess();
+            Console.WriteLine($"Count ==> {Numbers.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers.Capacity}");
+            Console.WriteLine();
+            List<int> Numbers2 = new List<int>(5) { 1,2,3,4,5};
+            Console.WriteLine($"Count ==> {Numbers2.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers2.Capacity}");
+            Console.WriteLine();
+            Numbers2.Add(6);
+            Console.WriteLine($"Count ==> {Numbers2.Count}");
+            Console.WriteLine($"Capacity ==> {Numbers2.Capacity}");
+            Console.WriteLine();
+            foreach(int Number in Numbers2)
+            {
+                Console.WriteLine(Number);
+            }
+            Console.WriteLine();
+            List<int> Numbers3 = new List<int>(5) {1,2,3,4,5};
+            //Numbers3.Add("Mostafa");
+            Console.WriteLine(Helper.SumList(Numbers3));
             #endregion
         }
     }
